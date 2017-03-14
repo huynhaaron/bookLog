566c0a0fed19c21010686c2ee0224a6af045cfc1
aaron huynh <huynh.aaron@gmail.com>
2017-03-14 15:06:11 -0700

    Add store

--- frontend/booklog.jsx
+++ frontend/booklog.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
 import { signup, login, logout } from './util/session_api_util';
 
 window.signup = signup;
@@ -7,6 +9,8 @@ window.login = login;
 window.logout = logout;
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
+    window.store = configureStore();
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Hi</h1>, root);
 });
