9f61bda2eea332639518db65dd09963aeb731450
aaron huynh <huynh.aaron@gmail.com>
2017-03-16 12:08:32 -0700

    Fix ErrorList DOM listings problem

--- frontend/components/error_list.jsx
+++ frontend/components/error_list.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 const ErrorList = ({ errors }) => {
   if (errors.length === 0) return null;
 
-  const errorItems = errors.map((error, idx) => <li key={ idx }>{ error }</li>);
+  const errorItems = errors.map((error, idx) => <li className="alert-text" key={ idx }>{ error }</li>);
 
   return (
     <div >
       <ul className="alert">
-        <li className="alert-text">{ errorItems }</li>
+        { errorItems }
       </ul>
     </div>
   )
