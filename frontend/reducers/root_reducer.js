e48eb2c8d8989a055dc1d185912d6d4585e9ce0b
aaron huynh <huynh.aaron@gmail.com>
2017-03-18 23:20:54 -0700

    Connect bookdetails reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,11 +3,14 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import BooksReducer from './books_reducer';
 import BookDetailReducer from './book_detail_reducer';
+import BookshelvesReducer from './book_detail_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   books: BooksReducer,
-  bookDetail: BookDetailReducer
+  bookDetail: BookDetailReducer,
+  bookshelves: BookshelvesReducer
+
 });
 
 export default RootReducer;
