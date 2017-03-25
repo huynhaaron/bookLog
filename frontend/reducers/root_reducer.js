df4d2c5444ba72b0db939fd9702f4ca2c7b4f957
aaron huynh <huynh.aaron@gmail.com>
2017-03-24 21:25:28 -0700

    Fix missing reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -5,13 +5,17 @@ import SessionReducer from './session_reducer';
 import BooksReducer from './books_reducer';
 import BookDetailReducer from './book_detail_reducer';
 import BookshelvesReducer from './bookshelves_reducer';
+import ReviewReducer from './review_reducer';
 import ShelvedbooksReducer from './shelvedbooks_reducer';
+import CurrentShelfReducer from './current_shelf_reducer';
 
 const appReducer = combineReducers({
   session: SessionReducer,
   books: BooksReducer,
   bookDetail: BookDetailReducer,
-  bookshelves: BookshelvesReducer
+  bookshelves: BookshelvesReducer,
+  currentShelf: CurrentShelfReducer,
+  reviews: ReviewReducer
 });
 
 const RootReducer = (state, action) => {
