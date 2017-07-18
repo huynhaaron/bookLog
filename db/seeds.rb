ad3cf6310ac9d8d8f78f81623593f10b7e7b3882
aaron huynh <huynh.aaron@gmail.com>
2017-07-18 11:27:17 -0700

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -476,6 +476,17 @@ Book.create!(title: "The Girl with the Dragon Tattoo",
           isbn: "0307269752",
           language: "en")
 
+Book.create!(title: "Harry Potter and the Prisoner of Azkaban",
+          author: "J.K. Rowling",
+          description: "Harry Potter's third year at Hogwarts is full of new dangers. A convicted murderer, Sirius Black, has broken out of Azkaban prison, and it seems he's after Harry. Now Hogwarts is being patrolled by the dementors, the Azkaban guards who are hunting Sirius. But Harry can't imagine that Sirius or, for that matter, the evil Lord Voldemort could be more frightening than the dementors themselves, who have the terrible power to fill anyone they come across with aching loneliness and despair. Meanwhile, life continues as usual at Hogwarts. A top-of-the-line broom takes Harry's success at Quidditch, the sport of the Wizarding world, to new heights. A cute fourth-year student catches his eye. And he becomes close with the new Defense of the Dark Arts teacher, who was a childhood friend of his father. Yet despite the relative safety of life at Hogwarts and the best efforts of the dementors, the threat of Sirius Black grows ever closer. But if Harry has learned anything from his education in wizardry, it is that things are often not what they seem. Tragic revelations, heartwarming surprises, and high-stakes magical adventures await the boy wizard in this funny and poignant third installment of the beloved series.",
+          cover_url: "https://images.gr-assets.com/books/1499277281l/5.jpg",
+          avg_rating: 4.53,
+          pages: 435,
+          publish_date: "2004-05-01",
+          publisher: "Scholastic",
+          isbn: "043965548X",
+          language: "en")
+
 
 
 Bookshelf.create!(user_id: 1, name: "sci-fi")
